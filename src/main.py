291d43f365ea94d0c5cd3fbe8f934e2e471e1233
IanCaru05 <ian.caruana.23@um.edu.mt>
2024-12-22 18:18:23 +0100

    Uploaded the updated main.py

--- src/main.py
+++ src/main.py
@@ -17,7 +17,9 @@ import pstats
 
 import threading
 from queue import Queue
+
 profiler = cProfile.Profile()
+
 
 def video_writer_thread(queue, video_writer):
     while True:
@@ -27,25 +29,29 @@ def video_writer_thread(queue, video_writer):
         video_writer.write(frame)
     video_writer.release()
 
+
 def distance(point1, point2):
-    return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+
 
 def parse_arguments():
-    parser = ArgumentParser(description="Inference script for detecting and recognising license plates using pre-trained YOLO and OCR model.")
+    parser = ArgumentParser(
+        description="Inference script for detecting and recognising license plates using pre-trained YOLO and OCR model.")
 
-    parser.add_argument("-i", "--input", type=str, required=False, help="Path to the input file.")
-    parser.add_argument("-o", "--output", type=str, required=False, help="Path to the output file.")
+    parser.add_argument("-i", "--input", type=str, required=True, help="Path to the input file or directory.")
+    parser.add_argument("-o", "--output", type=str, required=True, help="Path to the output directory.")
     parser.add_argument("-y", "--yolo", type=str, required=True, help="Path to the YOLO model.")
     parser.add_argument("-p", "--profiler", type=bool, required=False, help="If set to true, will profile the run.")
 
     return parser.parse_args()
 
+
 def run_detection_video(
-    yolo: YOLO,
-    ocr,
-    video_path: str,
-    output_path: str,
-    profile: bool
+        yolo: YOLO,
+        ocr,
+        video_path: str,
+        output_path: str,
+        profile: bool
 ) -> None:
     print("Creating video capture object ...")
     if profile:
@@ -57,26 +63,27 @@ def run_detection_video(
         if not video_capture.isOpened():
             exit("ERROR: Unable to read input data from camera source!")
             return
-    else:                   # input from file
-        video_capture = cv2.VideoCapture(video_path)    
+    else:  # input from file
+        video_capture = cv2.VideoCapture(video_path)
         if not video_capture.isOpened():
             print(f"ERROR: Unable to open video file for reading {video_path}.")
             return
-        
+
     frame_rate = int(video_capture.get(cv2.CAP_PROP_FPS))
     frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    
+
     start = time.time()
-    
+
     print("Video capture object created.")
     if output_path is not None:
         queue = Queue()
         print("Creating video writer object ...")
-        output_file_path = os.path.join(output_path, "output.avi")
+        base_name = os.path.splitext(os.path.basename(video_path))[0]
+        output_file_path = os.path.join(output_path, f"{base_name}_output.avi")
         video_writer = cv2.VideoWriter(
             output_file_path,
-            cv2.VideoWriter_fourcc('M','J','P','G'), 
+            cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
             frame_rate,
             (frame_width, frame_height),
         )
@@ -87,16 +94,16 @@ def run_detection_video(
             print(f"Error: Unable to open video file for writing {output_file_path}.")
             return
         print("Video writer object created.")
-    
+
     print("Detecting objects in the video...")
-    
-    ocr_results = defaultdict(list) 
-    aggregated_results = {}          
+
+    ocr_results = defaultdict(list)
+    aggregated_results = {}
     total_frames = 0
     try:
         while video_capture.isOpened():
             ret, frame = video_capture.read()
-            total_frames+=1
+            total_frames += 1
 
             if not ret:
                 break
@@ -110,15 +117,15 @@ def run_detection_video(
 
                 if object.boxes.id is not None:
                     track_id = int(object.boxes.id.item())
-                
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) 
 
-                    cropped_image = frame[y1:y2,x1:x2]
-                    text = run_recognition(cropped_image,ocr)
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+                    cropped_image = frame[y1:y2, x1:x2]
+                    text = run_recognition(cropped_image, ocr)
                     if track_id not in ocr_results or text != "":
                         ocr_results[track_id].append(text)
-                    
-                    aggregated_results[track_id] = max(set(ocr_results[track_id]), key=ocr_results[track_id].count) 
+
+                    aggregated_results[track_id] = max(set(ocr_results[track_id]), key=ocr_results[track_id].count)
 
                     cv2.rectangle(
                         frame,
@@ -137,22 +144,21 @@ def run_detection_video(
                         (0, 255, 0),
                         2,
                     )
-                
+
             if output_path is None:
                 cv2.imshow('Video', frame)
-                cv2.waitKey(1) 
+                cv2.waitKey(1)
             else:
                 queue.put(frame)
 
-    except KeyboardInterrupt: # Ctrl+C stops the processing
+    except KeyboardInterrupt:  # Ctrl+C stops the processing
         print("Interrupted! Ending ...")
     end = time.time()
 
     print()
-    print("Processing time:", "{:.3f}".format(end-start), "s")
+    print("Processing time:", "{:.3f}".format(end - start), "s")
     print("Processed frames:", total_frames)
-    print("Processing speed:", "{:.3f}".format(total_frames/(end-start)), "Fps")
-
+    print("Processing speed:", "{:.3f}".format(total_frames / (end - start)), "Fps")
 
     video_capture.release()
     if output_path is not None:
@@ -173,10 +179,11 @@ def run_detection_video(
             stats.sort_stats("calls")
             stats.print_stats(20)
 
-def run_recognition(image,ocr):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
+
+def run_recognition(image, ocr):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gaussian_image = cv2.GaussianBlur(gray_image, (3, 3), 0)
-    
+
     process_path = './output/preprocessed_license_plate.jpg'
     cv2.imwrite(process_path, gaussian_image)
 
@@ -186,29 +193,30 @@ def run_recognition(image,ocr):
         for line in result[0]:
             add_text = re.sub(r'[^a-zA-Z0-9]', '', line[0])
             conf = line[1]
-            if len(add_text) <=8 and conf > 0.5:
-                recognized_text += add_text 
+            if len(add_text) <= 8 and conf > 0.5:
+                recognized_text += add_text
 
     return recognized_text
 
+
 def run_detection_image(
-    yolo: YOLO,
-    ocr,
-    image_path: str,
-    output_path: str,
+        yolo: YOLO,
+        ocr,
+        image_path: str,
+        output_path: str,
 ) -> None:
     print("Detecting objects in the image...")
     image = cv2.imread(image_path)
     result = yolo(image_path)
-    
+
     for object in result[0]:
         x1, y1, x2, y2 = object.boxes.xyxy.cpu().squeeze()
         confidence = object.boxes.conf.item()
 
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) 
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-        cropped_image = image[y1:y2,x1:x2]
-        text = run_recognition(cropped_image,ocr)
+        cropped_image = image[y1:y2, x1:x2]
+        text = run_recognition(cropped_image, ocr)
 
         cv2.rectangle(
             image,
@@ -228,12 +236,14 @@ def run_detection_image(
             2,
         )
     if output_path is not None:
-        output_file_path = os.path.join(output_path, "output.png")
+        base_name = os.path.splitext(os.path.basename(image_path))[0]
+        output_file_path = os.path.join(output_path, f"{base_name}_output.png")
         cv2.imwrite(output_file_path, image)
     else:
         cv2.imshow("Image", image)
         cv2.waitKey(0)
     print("Detection done.")
+
 
 def main():
     args = parse_arguments()
@@ -243,27 +253,51 @@ def main():
     print("Model loaded.")
 
     print("Loading OCR model...")
-    ocr = PaddleOCR(use_gpu=False,lang="en",show_log=False, use_angle_cls=True)
+    ocr = PaddleOCR(use_gpu=False, lang="en", show_log=False, use_angle_cls=True)
     print("Model loaded.")
 
     profile = args.profiler
 
-    if args.input == None or args.input.split(".")[-1] == "mp4" :
-        run_detection_video(
-            yolo=yolo,
-            ocr=ocr,
-            video_path=args.input,
-            output_path=args.output,
-            profile=profile
-        )
+    if os.path.isdir(args.input):
+        for file in os.listdir(args.input):
+            file_path = os.path.join(args.input, file)
+            if file.lower().endswith(("mp4", "mov")):
+                run_detection_video(
+                    yolo=yolo,
+                    ocr=ocr,
+                    video_path=file_path,
+                    output_path=args.output,
+                    profile=profile
+                )
+            elif file.lower().endswith(("jpg", "jpeg", "png", "bmp")):
+                run_detection_image(
+                    yolo=yolo,
+                    ocr=ocr,
+                    image_path=file_path,
+                    output_path=args.output
+                )
+            else:
+                print(f"Unsupported file format: {file}")
     else:
-        run_detection_image(
-            yolo=yolo,
-            ocr=ocr,
-            image_path=args.input,
-            output_path=args.output
-        )
+        if args.input.lower().endswith(("mp4", "mov")):
+            run_detection_video(
+                yolo=yolo,
+                ocr=ocr,
+                video_path=args.input,
+                output_path=args.output,
+                profile=profile
+            )
+        elif args.input.lower().endswith(("jpg", "jpeg", "png", "bmp")):
+            run_detection_image(
+                yolo=yolo,
+                ocr=ocr,
+                image_path=args.input,
+                output_path=args.output
+            )
+        else:
+            print(f"Unsupported file format: {args.input}")
+            exit(1)
+
 
 if __name__ == "__main__":
     main()
-
